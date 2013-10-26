004fd23c8312650bdfde06306bd28ac39dd4a3f7
Ryoichi Ueno <nogami.ueno0515@gmail.com>
2013-10-26 17:32:54 +0900

    不要なもの削除
    次回
    フォームの形いじる

--- housebudget1.6.3/account_index.php
+++ housebudget1.6.3/account_index.php
@@ -42,53 +42,27 @@ require 'function/calculate_account_balance.php';
 	     
 	<div class = "center">
 		<a href="index.php">Back To TOP</a>
-	</div>
-     
-     <!-- 一覧部ここから -->   
-	<div class = "center">
-	<h2>口座情報</h2>
-	<table align = "center" >
-		<tr>
-			<th scope="col">ID</th>
-			<th scope="col">口座名</th>
-			<th scope="col">金額</th>
-			<th scope="col">編集</th>
-			<th scope="col">削除</th>
-		</tr>
-
-		<?php for ($i = 0; $i < count($account); $i++): ?>
 	
-		<tr>
-			<td><?php print(htmlspecialchars($account[$i]['id'], ENT_QUOTES));?></td>
-			<td><?php print(htmlspecialchars($account[$i]['name'], ENT_QUOTES));?></td>
-			<td><?php print(htmlspecialchars($account[$i]['balance'], ENT_QUOTES));?></td>
-			<td>
-				<form method = "POST" action = "account_update.php" >
-                 	<?php  //編集　id送信 ?>
-					<input type = "hidden" name = "id" value=<?php print(htmlspecialchars($account[$i]['id'], ENT_QUOTES));?> >
-					<input type = "submit" value = "編集" class="btn-primary" >
-                </form>
-            </td>
-            <td>   
-                <form method = "POST" action = "delete_action.php" >
-                 	<?php  //削除　収入キー送信　id送信 ?>
-					<input type = "hidden" name = "key" value="account" >
-					<input type = "hidden" name = "id" value=<?php print(htmlspecialchars($account[$i]['id'], ENT_QUOTES));?> >
-					<input type = "submit" value = "削除" class="btn-primary" onclick="return confirm('削除してよろしいですか');">
-                </form>
-			</td>
-		
-		</tr>
-		<?php endfor;?>
-	</table>
-	</div>
+	 <!-- 一覧部ここから -->   
+		<h2>口座情報</h2>
+		<table align = "center" >
+			<tr>
+				<th scope="col">口座名</th>
+				<th scope="col">金額</th>
+			</tr>
 
-	<div class = "center">
+			<?php for ($i = 0; $i < count($account); $i++): ?>
+			<tr>
+				<td><?php print(htmlspecialchars($account[$i]['name'], ENT_QUOTES));?></td>
+				<td><input type = "text" name = "balance" class="span2" style="text-align: right;" value="<?php print (htmlspecialchars($account[$i]['balance'],ENT_QUOTES));?>"/></td>
+			</tr>
+			<?php endfor;?>
+		</table>
+		
 		<a href="index.php">Back To TOP</a>
 	</div>
 
 	<!--一覧表示部終わり-->
-
 </body>
 </html>
 
